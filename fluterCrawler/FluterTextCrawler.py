12667178616e98d3d5041af3db19f33740c76cb9
Benedikt Beuttler <benedikt.beuttler@gmail.com>
2023-08-15 16:20:57 +0200

    updated code

--- fluterCrawler/FluterTextCrawler.py
+++ fluterCrawler/FluterTextCrawler.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import codecs
 import re
+import uuid
 
 basePath = "fluterCrawler"
 metaFileName = basePath + "/flutermeta.csv"
@@ -22,7 +23,7 @@ def logIt(text):
 
 def simpleSanitizeText(textToClean):
     # textToClean = " ".join(textToClean.split())
-    return textToClean.replace("·", "")
+    return re.sub(r'\s+', ' ', textToClean.strip().replace("Interview: ", "").replace("Fotos: ", "").replace("Text: ", "").replace('"', ''))
 
 def getNewsUrl(index):
     if index == 0:
@@ -45,23 +46,30 @@ def saveFile(fileName, text):
             f.write(f'{fileName}\n')
             f.close()
 
-def addToMetaFile(fileName, url, title, org, orgLink):
+def addToMetaFile(fileName, url, title, author, org, orgLink):
     if fileName in processedTexts:
         logIt("already exists in Meta: " + fileName)
         return
 
     # append entry to meta collection
     with codecs.open(metaFileName, 'a', encoding) as f:
-        f.write(f'\n{fileName};{title};;;{url};{fluterLizenz};;{org};{orgLink}')
+        f.write(f'\n{fileName}\t{title}\t{url}\t\t{fluterLizenz}\t{author}\t\t{org}\t{orgLink}')
         f.close()
 
     logIt(str("added to Meta - " + title))
 
-# crawler for full subpage news
-# todo: meta info rausnehmen und 
-
-# article body
-# relevant!!
+def crawlNormalText(soup):
+    text = ""
+    textContentHtml = soup.find_all("div", {"class": "articleBlock articleBlockStandard"})
+    for textContent in textContentHtml:
+        contentChildren = textContent.findChildren()
+        for contentChild in contentChildren:
+            if (contentChild.name=="div" and "articleBlock articleBlockInsertRightFloated" in contentChild.get("class", [])):
+                continue
+            if (contentChild.name=="p" and len(contentChild.get_text().strip())>1 ):
+                # merge texts
+                text += "\n" + contentChild.get_text().strip()
+    return text
 
 def crawlNewsPage(textUrl):
     r = requests.get(textUrl)
@@ -73,42 +81,84 @@ def crawlNewsPage(textUrl):
     org = simpleSanitizeText(
         soup.find("meta", property="og:site_name")["content"])
     orgLink = baseFluterUrl
-
-    # get text
-    textHeader = soup.find("div", {"class": "article__header"}).get_text()
-    textContent = soup.find("div", {"class": "article__content"})
-    colTextContents = textContent.find_all("div", {"class": "col-md-12"})
+    
+    authorHtml = soup.find("div", {"class": "author author-first"})
+    author = simpleSanitizeText(authorHtml.get_text()).replace(" und ", ", ")
+    textHeaderHtml = soup.find("div", {"class": "intro-fourth center <none>"})
+    textHeader = textHeaderHtml.find("h2").get_text().strip() + "."
     text = textHeader
-    for colTextContent in colTextContents:
-        contentWithH2 = colTextContent.find(
-            "h2", {"class": "background-title"})
-        # there is a h2 in content
-        if contentWithH2:
-            # print(colTextContent.findChild())
-            # does it start with h2 and if so, skip
-            if "h2" not in str(colTextContent.findChild()):
-                psInContent = colTextContent.find_all("p")
-                for pInContent in psInContent:
-                    if len(pInContent.get_text()) > 1:
-                        # add it
-                        text += "\n" + pInContent.get_text().strip()
+    
+    # check if its a disput text by checking tags for "streit"
+    disput = False
+    tagsHtmls = soup.find_all("ul", {"class":"tags-second"})
+    for tagHtml in tagsHtmls:
+        if "streit" in tagHtml.get_text().strip().lower():
+            disput = True
             break
-        # merge texts
-        text += "\n" + colTextContent.get_text().strip()
+        
+    if disput:
+        # different crawling here
+        
+        texts = soup.find_all("div", {"class": "left_floated"})
+        i = 0
+        for textDisput in texts:
+            specAuthor = ""
+            textChildren = textDisput.findChildren()
+            for textChild in textChildren:
+                if (textChild.name == "h2" and "element-invisible" in textChild.get("class", [])):
+                    continue
+                elif (textChild.name == "h2"):
+                    text += "\n" + textChild.get_text().strip() + "."
+                if (textChild.name == "h3"):
+                    text += "\n" + textChild.get_text().strip() + "."
+                if (textChild.name=="div" and "media media-element-container media-default" in textChild.get("class", [])):
+                    continue
+                if (textChild.find("em")):        
+                    continue
+                if (textChild.name == "p"):
+                    text += "\n" + textChild.get_text().strip()
+            
+            specAuthor = author.split(", ")[i]
+            # print(specAuthor)
+            i += 1
+            
+            fileName = re.sub('[^A-Za-zäöüÄÖÜß0-9]+', '', str(title + specAuthor)) + ".txt" 
+            text = simpleSanitizeText(re.sub(r'([\r\n])+', r'\r\n', text))
 
-    fileName = re.sub('[^A-Za-zäöüÄÖÜß0-9]+', '', title) + ".txt"
-    text = simpleSanitizeText(re.sub(r'([\r\n])+', r'\r\n', text))
+            if len(text) < 1:
+                return
+            
+            while len(text[0].strip()) < 1:
+                text = text[1:]
 
-    while len(text[0].strip()) < 1:
-        text = text[1:]
+            saveFile(fileName, text)
+            addToMetaFile(fileName, textUrl, title, specAuthor, org, orgLink)
+            # print("")
+            # print(text)
+            # print(fileName)
+            # print(textUrl, title, author, org, orgLink)
+            # print("------------------------------------")
+    else:
+        # normal text crawling
+        # get text    
+        text += "\n" + crawlNormalText(soup)
+    
+        fileName = re.sub('[^A-Za-zäöüÄÖÜß0-9]+', '', title) + ".txt"
+        text = simpleSanitizeText(re.sub(r'([\r\n])+', r'\r\n', text))
 
-    saveFile(fileName, text)
-    addToMetaFile(fileName, textUrl, title, org, orgLink)
-    # print("")
-    # print(text)
-    # print(fileName)
-    # print(textUrl, title, org, orgLink)
-    # print("------------------------------------")
+        if len(text) < 1:
+            return
+        
+        while len(text[0].strip()) < 1:
+            text = text[1:]
+
+        saveFile(fileName, text)
+        addToMetaFile(fileName, textUrl, title, author, org, orgLink)
+        # print("")
+        # print(text)
+        # print(fileName)
+        # print(textUrl, title, author, org, orgLink)
+        # print("------------------------------------")
 
 # clear log    
 with codecs.open(logs, 'w', encoding) as f:
@@ -123,15 +173,20 @@ with codecs.open(processedTextsFile, "r", encoding) as f:
 
 # read processed files first and save in array
 # for testing purposes only have a look at the first page
-for i in range(0,1):
-    logIt(str(i))
+for i in range(0, 404): #404
     # for testing purposes, i am crawling here the articles directly and break afterwards.
-    # crawlNewsPage("https://www.sr.de/sr/home/nachrichten/nachrichten_einfach/schriftgroesse_kontrast_nachrichten_einfach100.html")
-    crawlNewsPage("https://www.fluter.de/polizeigewalt-kennzeichungspflicht-behr")
-    break
+    # crawlNewsPage("https://www.fluter.de/wm-katar-boykottieren-streit")
+    # crawlNewsPage("https://www.fluter.de/unterwassergarten-genua-landwirtschaft")
+    # crawlNewsPage("https://www.fluter.de/polizeigewalt-kennzeichungspflicht-behr")
+    # exit()
     url = getNewsUrl(i)
+    logIt(getNewsUrl(i))
 
-    req = requests.get(url)
+    headers = {
+                    "X-Request-ID": str(uuid.uuid4()),
+                    "From": "https://www.germ.univie.ac.at/projekt/latill/"
+                }
+    req = requests.get(url, headers = headers)
     dataHtml = req.text
     soup = BeautifulSoup(dataHtml, features="html.parser")
 
@@ -146,5 +201,5 @@ for i in range(0,1):
         link = newsDiv.find("a", href=True)
         if link is not None:
             completeLink = baseFluterUrl + link['href']
-            print(completeLink)
-        # crawlNewsPage(completeLink)
+            logIt("checking:" + completeLink)
+            crawlNewsPage(completeLink)
--- geolinoCrawler/GeolinoTextCrawler.py
+++ geolinoCrawler/GeolinoTextCrawler.py
@@ -5,15 +5,18 @@ import requests
 import codecs
 import re
 import os
+import uuid
 
 basePath = "geolinoCrawler"
 metaFileName = basePath + "/geolinometa.csv"
-baseGeolinoUrl = "https://www.geo.de"
+baseGeolinoUrl = "https://www.geolino.de"
+author = "Geolino"
 processedTextsFile = basePath + "/processed-geolino-files.txt"
 logs = basePath + "/logs.txt"
 geolinoLizenz = "unknown"
-allowedCategories = ["berufe", "forschung-und-technik", "natur-und-umwelt",
-                     "mensch", "redewendungen", "filmtipps", "spieletests"]
+disallowedCategories = ["kreativ, wettbewerbe, archive, spiele"]
+# allowedCategories = ["berufe", "forschung-und-technik", "natur-und-umwelt",
+#                      "mensch", "redewendungen", "filmtipps", "spieletests"]
 encoding = "utf-8"
 
 
@@ -25,7 +28,7 @@ def logIt(text):
 
 def simpleSanitizeText(textToClean):
     # textToClean = " ".join(textToClean.split())
-    return textToClean.replace("·", "")
+    return textToClean
 
 def getNewsUrl(month, year):
     newsUrl = "https://www.geo.de/geolino/archiv/?month=" + str(month) + "&year=" + str(year)
@@ -52,7 +55,7 @@ def addToMetaFile(fileName, url, title, org, orgLink):
 
     # append entry to meta collection
     with codecs.open(metaFileName, 'a', encoding) as f:
-        f.write(f'\n{fileName};{title};;;{url};{geolinoLizenz};;{org};{orgLink}')
+        f.write(f'\n{fileName}\t{title}\t{url}\t\t{geolinoLizenz}\t{author}\t\t{org}\t{orgLink}')
         f.close()
 
     logIt(str("added to Meta - " + title))
@@ -69,12 +72,6 @@ def cleanupBeforehand():
                 f.write("")
                 f.close()
 
-# crawler for full subpage news
-# todo: meta info rausnehmen und 
-
-# article body
-# relevant!!
-
 def crawlNewsPage(textUrl):
     r = requests.get(textUrl)
     data = r.text
@@ -85,42 +82,55 @@ def crawlNewsPage(textUrl):
     org = simpleSanitizeText(
         soup.find("meta", property="og:site_name")["content"])
     orgLink = baseGeolinoUrl
-
+    
     # get text
-    textHeader = soup.find("div", {"class": "article__header"}).get_text()
-    textContent = soup.find("div", {"class": "article__content"})
-    colTextContents = textContent.find_all("div", {"class": "col-md-12"})
-    text = textHeader
-    for colTextContent in colTextContents:
-        contentWithH2 = colTextContent.find(
-            "h2", {"class": "background-title"})
-        # there is a h2 in content
-        if contentWithH2:
-            # print(colTextContent.findChild())
-            # does it start with h2 and if so, skip
-            if "h2" not in str(colTextContent.findChild()):
-                psInContent = colTextContent.find_all("p")
-                for pInContent in psInContent:
-                    if len(pInContent.get_text()) > 1:
-                        # add it
-                        text += "\n" + pInContent.get_text().strip()
-            break
-        # merge texts
-        text += "\n" + colTextContent.get_text().strip()
+    text = ""
+    # skip podcasts
+    podcast = soup.find("div", {"class": "title article__title"})
+    if (podcast and "podcast" in podcast.get_text().strip().lower() ):
+        return
+    content = soup.find("div", {"class": "article__body js-article-body"})
+    if content == None:
+        content = soup.find("div",{"class":"gallery page__inner-content"})
+    contentChildren = content.findChildren()
+    for contentChild in contentChildren:
+        skip = True
+        addDot = False
+        # print(contentChild)
+        # subtitle
+        class_name_pattern = re.compile(r'\btext-element\b', re.IGNORECASE)
 
+        if (contentChild.name == "div" and "authors article__authors" in contentChild.get("class", [])):
+            skip = True
+        if (contentChild.name == "div" and "intro" in contentChild.get("class", [])):
+            skip = False
+            addDot = True
+        if (contentChild.name=="div" and any(class_name_pattern.search(cls) for cls in contentChild.get("class", []))):
+            skip = False
+        if (contentChild.name=="p" and any(class_name_pattern.search(cls) for cls in contentChild.get("class", []))):
+            skip = False
+            
+        if not skip:
+            if addDot:
+                text += "\n" + contentChild.get_text().strip() + "."
+            else:
+                text += "\n" + contentChild.get_text().strip()
+        else:
+            continue
+    
+    # print(text)
+    
     fileName = re.sub('[^A-Za-zäöüÄÖÜß0-9]+', '', title) + ".txt"
     text = simpleSanitizeText(re.sub(r'([\r\n])+', r'\r\n', text))
+    
+    if len(text) < 1:
+        return
 
     while len(text[0].strip()) < 1:
         text = text[1:]
 
     saveFile(fileName, text)
     addToMetaFile(fileName, textUrl, title, org, orgLink)
-    # print("")
-    # print(text)
-    # print(fileName)
-    # print(textUrl, title, org, orgLink)
-    # print("------------------------------------")
 
 cleanupBeforehand()
             
@@ -130,17 +140,22 @@ with codecs.open(processedTextsFile, "r", encoding) as f:
         processedTexts.append(line.strip())
 
 # year
-for i in range(0, 24): # 24
+for i in range(11, 24): # 24
     # month
     for j in range(1, 13): #13
         logIt(str("year: " + str(i) + " month: " + str(j)))
         # crawlNewsPage("https://www.geo.de/geolino/natur-und-umwelt/10697-rtkl-wanzen-milben-zecken-wer-auf-uns-so-alles-wohnt")
-        # crawlNewsPage("https://www.geo.de/geolino/mensch/10006-rtkl-die-schule-der-kung-fu-maedchen")
+        # crawlNewsPage("https://www.geo.de/geolino/natur-und-umwelt/9712-rtkl-fotoshow-geo-tag-der-artenvielfalt-2005")
+        # crawlNewsPage("https://www.geo.de/geolino/wissen/fuer-kinder-erklaert--krieg-in-der-ukraine-31690312.html")
         
-        # break
+        # exit()
         url = getNewsUrl(j, 2002 + i)
         
-        req = requests.get(url)
+        headers = {
+                    "X-Request-ID": str(uuid.uuid4()),
+                    "From": "https://www.germ.univie.ac.at/projekt/latill/"
+                }
+        req = requests.get(url, headers=headers)
         dataHtml = req.text
         soup = BeautifulSoup(dataHtml, features="html.parser")
 
@@ -155,10 +170,23 @@ for i in range(0, 24): # 24
             link = newsDiv.find("a", href=True)
             if link is not None:
                 completeLink = link['href']
-                # only allow certain categories
-                for cat in allowedCategories:
-                    # print(cat + " in " + completeLink)
-                    if str("/" + cat + "/") in completeLink:
-                        # print(completeLink)
-                        print(cat + " in " + completeLink)
-                        # crawlNewsPage(completeLink)
+                logIt("checking... " + completeLink)
+                # skip stuff
+                substrings_to_check = ["/geolino/1", "podcast", "hoerspiel", "-quiz-", "wissenstest", "fotoquiz", "-video-"]
+                if any(substring in completeLink for substring in substrings_to_check):
+                    logIt("skipping: " + completeLink)
+                    continue
+                
+                pattern = r"https://www.geo.de/geolino/([^/]+)" # gives category string
+                match = re.search(pattern, completeLink)
+                if match:
+                    extracted_string = match.group(1)
+                    if extracted_string not in disallowedCategories:
+                        crawlNewsPage(completeLink)
+                        # print(extracted_string)
+                    else:
+                        logIt("skipping: " + completeLink)
+                        continue
+                else:
+                    logIt("Pattern not found in the URL: " + completeLink)
+                    continue
