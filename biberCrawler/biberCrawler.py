4229e15ba4661a69603eeb162e0accd0e0829eda
Paula Wicht <paulawicht@gmail.com>
2023-08-31 14:56:17 +0200

    biberCrawler.   3 problems with writing to files

--- biberCrawler/biberCrawler.py
+++ biberCrawler/biberCrawler.py
@@ -9,13 +9,14 @@ import uuid
 # Variables which have the same value for all articles
 basePath = "biberCrawler"
 metaFileName = basePath + "/bibermeta.csv"
+metaFile2 = basePath + "biber_article_overview.csv"
 logs = basePath + "/logs.txt"
 biberLizenz = 'unknown'
 encoding = "utf-8"
 author = 'dasBiber'
 org = 'dasBiber'# 
 orgLink = 'https://www.dasbiber.at/'
-processed_files = basePath +  "processed-biber-files.txt"
+processed_files = basePath +  "processed_biber_files.txt"
 
 
 def write_to_logs(url):
@@ -30,14 +31,20 @@ def write_to_processedFiles(fileName):
         f.close()
 
 
-def addToMetaFile(meta_title, url, cleaned_bereich, fileName):
+def addToMetaFile(meta_title, url, fileName):
     with codecs.open(metaFileName, 'a', encoding) as f:
-        f.write(f'{meta_title}\t{url}\t{cleaned_bereich}\t{fileName}\t{biberLizenz}\t{author}\t\t{org}\t{orgLink}\n')
-        f.close()
+        f.write(f'{fileName}\t{meta_title}\t{url}\t\t{biberLizenz}\t{author}\t{author}\t{org}\t{orgLink}\n')
+        f.close
+
+def addToMetaFile2(meta_title, url, cleaned_bereich):        
+    with codecs.open(metaFile2, 'a', encoding) as p: 
+        p.write(f'{meta_title}, {url}, {cleaned_bereich}')
+        p.close()
 
 
 def safe_text_in_file(fileName, text):
-     with codecs.open(fileName, 'a', encoding) as f:
+     actual_fileName = basePath+ "/txts/" + fileName
+     with codecs.open(actual_fileName, 'a', encoding) as f:
         f.write(text)
         f.close()
 
@@ -53,34 +60,36 @@ def crawl_page_meta_and_text(url__link):
                     "From": "https://www.germ.univie.ac.at/projekt/latill/"
 
                 }
-     req = requests.get(url, headers = headers)
-     data = r.text
+     req = requests.get(url__link, headers = headers)
+     data = req.text
      soup = BeautifulSoup(data, features="html.parser")
      
      # search meta data 
      meta_title = soup.title
      meta_title = meta_title.string 
      meta_title = meta_title[:-11] 
-     url = (soup.find('link', rel=re.compile('canonical'))['href']) 
+     #url = (soup.find('link', rel=re.compile('canonical'))['href']) 
 
 
      # get Bereich. Wo ist Bereich?
      # <div class="field field-name-field-bereich field-type-taxonomy-term-reference field-label-above clearfix">
      bereich = soup.find("div", {"field field-name-field-bereich field-type-taxonomy-term-reference field-label-above clearfix"})
-     if (type(bereich)!= None):
-        ("tag with bereich found ")
+     if (bereich is not None):
         bereich = bereich.get_text()
         cleaned_bereich = re.sub(r'\n', ' ', bereich)
+        ("tag with bereich found " + cleaned_bereich)
      else: 
          cleaned_bereich = "no specified category"
          print(cleaned_bereich)
 
      clean_fileName  = re.sub('[^a-zA-Z0-9äöüÄÖÜ \n\.]', " ", meta_title)
-     fileName = basePath+ "/txts/" + clean_fileName.replace(" ", "") + '.txt'
+     real_fileName = clean_fileName.replace(" ", "") + '.txt'
      
      
     # add results to metacsv - maybe later ? 
-     addToMetaFile(meta_title, url, cleaned_bereich, fileName)
+     print(meta_title, cleaned_bereich, real_fileName, url__link)
+     addToMetaFile(meta_title, url__link,  real_fileName)
+     addToMetaFile2(meta_title, url__link, cleaned_bereich)
 
     # find text
      text = ""
@@ -88,8 +97,6 @@ def crawl_page_meta_and_text(url__link):
          
      contentChildren = parent.findChildren(recursive=False)
      for child in contentChildren:   
-        # print(child)
-        # print("----------")
 
         ## TODOs
         # abfrage wenn p leer ist (kein text)
@@ -102,15 +109,15 @@ def crawl_page_meta_and_text(url__link):
             
            
     # safe text in for it created file
-     safe_text_in_file(fileName, text)
-     write_to_processedFiles(fileName)
+     safe_text_in_file(real_fileName, text)
+     write_to_processedFiles(real_fileName)
 
 
 def Crawl():
     
     
     basePageURL = "https://www.dasbiber.at/articles"
-    for i in range(0,320):
+    for i in range(0,221):
         if i == 0:
             get_each_URL1(basePageURL)
         else:
@@ -118,6 +125,11 @@ def Crawl():
             newPageURL = basePageURL + "?page=" +str(i)
 
             get_each_URL1(newPageURL)
+            if ( i == 50) or (i == 100) or (i == 200):
+                print("reached page: " + i)
+            else: 
+                continue
+
 
 
 def get_each_URL1(newPageURL ):
